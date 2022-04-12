fe99339d2ed0e79ae788fc3997e17a02a66b85d7
Maurice Yang <mauriceyangmy28@gmail.com>
2022-04-12 14:13:55 +0700

    Made it into a PWA

--- next.config.js
+++ next.config.js
@@ -1,9 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+const withPWA = require('next-pwa')
+
+const nextConfig = withPWA({
     reactStrictMode: true,
     images: {
         domains: ['lh3.googleusercontent.com'],
+    },
+    pwa: {
+        dest: "public",
+        register: true,
+        skipWaiting: true,
+        disable: process.env.NODE_ENV === 'development'
     }
-}
+})
 
 module.exports = nextConfig
